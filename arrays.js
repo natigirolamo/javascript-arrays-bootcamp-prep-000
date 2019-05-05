e2123420f2ed427dda8aa13028df9533591ef892
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-05 23:00:42 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,28 +1,3 @@
-var ingredient1 = "bread"
-var ingredient2 = "mild cheese"
-var ingredient3 = "sharp cheese"
-var ingredient4 = "butter"
-var ingredient5 = "garlic"
-var ingredient6 = "tomato"
-var ingredient7 = "garlic"
-
-var grilledCheeseIngredients = [
-  'bread',
-  'mild cheese',
-  'sharp cheese',
-  'butter',
-  'tomato',
-  'garlic'
-]
- 
-var tomatoSauceIngredients = [
-  'tomato',
-  'garlic',
-  'olive oil',
-  'basil',
-  'oregano'
-]
-
 var chocolateBars = [
   'snickers',
   'hundred grand',
