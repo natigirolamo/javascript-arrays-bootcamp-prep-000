4224cc54fb1e53a210bfd904ebdfcd0467b73b33
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-05 23:00:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -39,4 +39,32 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars)
   return chocolateBars
 }
 function addElementToEndOfArray (chocolateBars, twix)
-  return [...chocolateBars, candyString]; return []
+  return [...chocolateBars, twix]; 
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
+  chocolateBars.push(candyString);
+  return chocolateBars;
+}
+ 
+function accessElementInArray(array, index){
+  return array[index];
+}
+ 
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+  chocolateBars.shift();
+  return chocolateBars;
+}
+ 
+function removeElementFromBeginningOfArray(chocolateBars){
+  return chocolateBars.slice(1);
+}
+ 
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  chocolateBars.pop();
+  return chocolateBars;
+}
+ 
+function removeElementFromEndOfArray(chocolateBars){
+  return chocolateBars.slice(0, chocolateBars.length - 1);
+}
